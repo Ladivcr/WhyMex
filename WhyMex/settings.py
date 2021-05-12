fb2b1440c72090722207964a1a7633e13ab8e0df
Ladivcr <vrosas832@gmail.com>
2021-05-11 23:27:09 -0500

    instale django_heroku con pip y añadi unas cosas al setting

--- WhyMex/settings.py
+++ WhyMex/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +154,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django-Heroku.
+django_heroku.settings(locals())
