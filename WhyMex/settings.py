9e124d60fbbec98173f6a47a50376f3b77cb63d9
Ladivcr <vrosas832@gmail.com>
2021-05-14 21:13:07 +0000

    Sitio en produccion

--- WhyMex/settings.py
+++ WhyMex/settings.py
@@ -68,12 +68,24 @@ WSGI_APPLICATION = 'WhyMex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'WhyMex$wmproblems',
+            'USER': 'WhyMex',
+            'PASSWORD': '3JJeRd7DvE9vLci',
+            'HOST': 'WhyMex.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
     }
-}
+
 
 
 # Password validation
