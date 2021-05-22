3043f886f2bd066971819b34c94959e998eb962a
Ladivcr <vrosas832@gmail.com>
2021-05-21 22:59:01 -0500

    ocultar secret_key

--- WhyMex/settings.py
+++ WhyMex/settings.py
@@ -9,8 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+#! Obtencio de datos sensibles
+config = dotenv_values("/home/WhyMex/WhyMex/.env")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cd3mb9-1+l7vl25$2ox4*s=9fzp0*wr80q3y6!q2xmj1id0x91'
+SECRET_KEY = config['SECRETKEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -67,7 +70,7 @@ WSGI_APPLICATION = 'WhyMex.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-config = dotenv_values("/home/WhyMex/WhyMex/.env")
+
 
 DATABASES = {
         'default': {
